e63beb685d70c79f5a432549a3322cfca1861ca9
Katianne Araujo <71135012+katianne23@users.noreply.github.com>
2021-11-07 18:48:12 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,7 +1,7 @@
 export const config = {
     API_URL: 'http://localhost:5000',
     MONGO_CONNECTION:
-    'mongodb+srv://admin:<password>@url-shortener-dio.kr37q.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+   'mongodb+srv://<user>:<password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
 }
 
 
