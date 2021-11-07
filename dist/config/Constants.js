e5fe9b905fc7a2f9a7ceb28ea474ad59962a5b11
katianne23 <katianne40@gmail.com>
2021-11-07 18:16:33 -0300

    Url Shortener

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 'mongodb+srv://admin:Katianne2021@url-shortener-dio.kr37q.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,7 @@
+export const config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION:
+    'mongodb+srv://admin:<password>@url-shortener-dio.kr37q.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+}
+
+
